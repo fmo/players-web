ac4d7b3be5a7ffc3e7b8fe70b58e5b0a1d611839
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2024-07-17 13:07:22 +0200

    list players with order

--- js/main.js
+++ js/main.js
@@ -5,8 +5,48 @@ document.addEventListener("DOMContentLoaded", function() {
         let response = await fetch("http://localhost:8112/players?teamId=" + selectedTeamId)
         let { data } = await response.json()
 
-        data.forEach((player) => {
-            document.getElementById('result').innerHTML += player.firstname + " " + player.lastname + "<br />";
-        });
+        let playersByPosition = groupPlayersByPosition(data)
+        let orderedPlayers = orderPlayersByPosition(playersByPosition);
+
+        displayPlayers(orderedPlayers)
     });
 });
+
+function groupPlayersByPosition(players) {
+    let playersByPosition = {};
+
+    players.forEach((player) => {
+        if (!playersByPosition[player.position]) {
+            playersByPosition[player.position] = []
+        }
+        playersByPosition[player.position].push(player)
+    })
+
+    return playersByPosition
+}
+
+function orderPlayersByPosition(playersByPosition) {
+    const positionOrder = ["Goalkeeper", "Defender", "Midfielder", "Attacker"];
+    let orderedPlayers = {};
+
+    positionOrder.forEach(position => {
+        if (playersByPosition[position]) {
+            orderedPlayers[position] = playersByPosition[position];
+        }
+    });
+
+    return orderedPlayers
+}
+
+function displayPlayers(orderedPlayers) {
+    let resultElement = document.getElementById('result');
+    resultElement.innerHTML = '';
+
+    Object.keys(orderedPlayers).forEach(position => {
+        resultElement.innerHTML += `<h3>${position}</h3>`;
+
+        orderedPlayers[position].forEach(player => {
+            resultElement.innerHTML += `${player.firstname} ${player.lastname}<br />`;
+        });
+    });
+}
